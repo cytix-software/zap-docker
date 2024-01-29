a10a5878e278214ed62f92023f943ba5924de00b
Amy <amy@cytix.io>
2024-01-29 14:18:36 +0000

    📝 Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Zap YML Injector
+- ⚠️ Environment Variable `API_KEY` must be set before running. This is used to authorise requests to the server.
+- Optionally, the variable `FILE_PATH` can be set to determine the location/name of the YML file. By default this is `./output.yml`.
+
+To start the server, run `npm start`.
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import { writeFile } from 'fs/promises'
 import generateYaml from './generateYaml'
 dotenv.config()
-if (!process.env.ZAP_API_KEY) throw new Error('\x1b[31mEnvironment variable "ZAP_API_KEY" not set. This is required for authorising incoming requests.\x1b[0m')
+if (!process.env.API_KEY) throw new Error('\x1b[31mEnvironment variable "ZAP_API_KEY" not set. This is required for authorising incoming requests.\x1b[0m')
 if (!process.env.FILE_PATH) throw new Error('\x1b[31mEnvironment variable "FILE_PATH" not set. This is required for saving the YML file to a custom path.\x1b[0m')
 
 const app = express()
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.post('/yaml', async (req, res, next) => {
   // Check Authorization 
-  if (req.headers.authorization !== process.env.ZAP_API_KEY) {
+  if (req.headers.authorization !== process.env.API_KEY) {
     return res.sendStatus(401)
   }
 
