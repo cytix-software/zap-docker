db46679c72786c6cec7b7ca2fe4d48e503f12ba5
Amy <amy@cytix.io>
2024-01-29 11:50:50 +0000

    ✨ Generate YML file from input JSON data

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,128 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { readFile, writeFile } from 'fs/promises'
+import yaml from 'yaml'
+import util from 'util'
 dotenv.config()
-if (!process.env.ZAP_API_KEY) throw new Error('\x1b[31mEnvrionment variable "ZAP_API_KEY" not set. This is required for authorising incoming requests.\x1b[0m')
+if (!process.env.ZAP_API_KEY) throw new Error('\x1b[31mEnvironment variable "ZAP_API_KEY" not set. This is required for authorising incoming requests.\x1b[0m')
+if (!process.env.FILE_PATH) throw new Error('\x1b[31mEnvironment variable "FILE_PATH" not set. This is required for saving the YML file to a custom path.\x1b[0m')
 
 const app = express()
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.send('Healthy')
 })
 
-app.post('/yaml', (req, res) => {
+app.post('/yaml', async (req, res) => {
   // Check Authorization 
   if (req.headers.authorization !== process.env.ZAP_API_KEY) {
     return res.sendStatus(401)
   }
 
   // Validate body
+  const body = <YamlRequest>req.body
+  if (!body.username || !body.password) {
+    res.statusCode = 400
+    return res.json({ message: 'Username or password fields are undefined. Username and password are required in the request at a minimum.' })
+  }
+  if (!body.url) {
+    res.statusCode = 400
+    return res.json({ message: `"url" paramter wasn't supplied.` })
+  }
 
   // Generate YAML file from body
+  const template = <YamlTemplate>yaml.parse(await readFile('./src/template.yml', 'utf-8'))
+  template.env.contexts[0].urls = [ body.url ]
+  template.env.contexts[0].includePaths = [ body.url + '.*' ]
+
+  template.env.contexts[0].users[0] = {
+    name: 'User',
+    credentials: {
+      username: body.username,
+      password: body.password
+    }
+  }
+
+  if (body.loginUrl) {
+    template.env.contexts[0].authentication.parameters = {
+      browserId: 'firefox-headless',
+      loginPageUrl: body.loginUrl,
+      loginPageWait: 10
+    }
+  }
+  if (body.pollUrl) {
+    template.env.contexts[0].authentication.verification = {
+      method: 'poll',
+      pollUrl: body.pollUrl,
+      loggedInRegex: body.loggedInRegex || '',
+      loggedOutRegex: body.loggedOutRegex || '',
+      pollFrequency: 60,
+      pollUnits: 'requests',
+      pollPostData: ''
+    }
+  }
 
   // Save to path set in environment variables
-
-  // Return YML file ?
-
-  return res.sendStatus(204)
+  const yml = yaml.stringify(template)
+  await writeFile(process.env.FILE_PATH || './output.yml', yml)
+  return res.send(yml)
 })
 
 app.listen(80, () => {
   console.log(`\n\x1b[33mServer running at \x1b[36mhttp://localhost:80\x1b[0m`)
 })
+
+export interface YamlRequest {
+  username: string
+  password: string
+  url: string
+  loginUrl?: string
+  pollUrl?: string
+  loggedInRegex?: string
+  loggedOutRegex?: string
+}
+
+interface YamlTemplate {
+  env: {
+    contexts: {
+      name: string
+      urls?: string[]
+      includePaths?: string[]
+      excludePaths: string[]
+      authentication: {
+        method: 'browser'
+        parameters?: {
+          loginPageUrl: string
+          loginPageWait: number
+          browserId: 'firefox-headless'
+        }
+        verification?: {
+          method: 'poll'
+          loggedInRegex: string
+          loggedOutRegex: string
+          pollFrequency: number
+          pollUnits: 'requests'
+          pollUrl: string
+          pollPostData: string
+        }
+      }
+      sessionManagement: {
+        method: string
+      }
+      users: {
+        name: string
+        credentials: {
+          username: string
+          password: string
+        }
+      }[]
+    }[]
+    parameters: {
+      failOnError: boolean
+      failOnWarning: boolean
+      progressToStdout: boolean
+    }
+  }
+  jobs: any[]
+}
